d6348f19e9825b429b23e34dc771235c36ef05c5
Kiri Um <kirirotha@gmail.com>
2020-07-16 12:59:38 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
+    if self.end_with?("!") == true
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
+    split_para = self.split(/[.!?]/)
+    split_para = split_para.reject { |item| item == ""}
+    sentence_count = split_para.count
 
   end
 end
