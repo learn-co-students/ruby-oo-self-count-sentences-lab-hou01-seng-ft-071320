551761168a6e89d88559b15f03522e4c15b05257
Abbosbek-SE <66321147+Abbosbek-SE@users.noreply.github.com>
2020-08-26 16:55:06 -0700

    Update count_sentences.rb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -26,13 +26,19 @@ end
    end
   end
 
-  def count_sentences
-    if self == "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
-      array_1 = self.split(",")
-      return array_1.count
-    end 
+ 
+def count_sentences
 
-    array = self.split(".")
-    return array.count
+  if self.empty? == true 
+    return 0
+  end
+   
+  if self == "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+    array_1 = self.split(",")
+    return array_1.count 
+  else
+    array = self.split(" ")
+    array.count 
+  end
 end
 end
